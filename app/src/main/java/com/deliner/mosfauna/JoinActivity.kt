9ece89ef7eb44205419629ff5cdc64bfecf0858e
Deliner <deliner.work@gmail.com>
2020-09-05 17:38:10 +0300

    main

---
+++ app/src/main/java/com/deliner/mosfauna/JoinActivity.kt
@@ -0,0 +1,17 @@
+package com.deliner.mosfauna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/deliner/mosfauna/system/MosFaunaApp.kt
@@ -0,0 +1,6 @@
+package com.deliner.mosfauna.system
+
+import androidx.multidex.MultiDexApplication
+
+class MosFaunaApp: MultiDexApplication() {
+}
